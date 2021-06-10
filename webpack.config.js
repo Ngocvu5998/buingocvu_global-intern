fa07415d6d7c850a0849c015f884eed39f9cc1ce
buingocvuktkt@gmail.com <buingocvuktkt@gmail.com>
2021-06-10 12:40:15 +0700

    demo

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+let mode = "development";
+if(process.env === "production") {
+    mode = "production";
+}
+module.exports = {
+    mode: mode,
+    entry:[
+        './asset/scss/main.scss',
+		'./asset/js/main.js'
+    ],
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'js/main.bundle.js'
+    },
+    module: {
+        rules: 
+        [
+            {
+                test: /\.js%/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.(scss|css)$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader',  'sass-loader'],
+            },
+            {
+				test: /\.(png|jpg)$/i,
+				use: [
+					'file-loader?name=[name].[ext]&outputPath=dist/imgs/&publicPath=../imgs/',
+					'image-webpack-loader'
+				]
+			},
+            {
+				test: /\.(svg|woff|woff2|eot|otf|TTF)$/,
+				use: [
+					'file-loader?name=[name].[ext]&outputPath=fonts/&publicPath=../'
+				]
+			}
+        ]
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname,"dist"),
+        hot: true,
+    },
+    plugins: [
+        new MiniCssExtractPlugin({filename:'css/style.bundle.css'}),
+    ]
+}
